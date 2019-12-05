4dbe01a277377d3685cb358a45664361fc19b2e0
Kelvin <kt16@sanger.ac.uk>
2019-12-05 12:07:12 +0000

    first commit

---
+++ bin/getSRA.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+while getopts ":l:" opt; do
+  case $opt in
+    l)
+      cat $OPTARG | xargs -n 1 bash pre_fetch.sh >&2
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
+while getopts ":l:" opt; do
+  case $opt in
+    l)
+      cat $OPTARG | xargs -n 1 bash fastq_dump.sh >&2
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
---
+++ bin/pre_fetch.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+prefetch -v $1
