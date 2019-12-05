3c3ab0f57c065550af659279acf98c0db1e6434b
Kelvin <kt16@sanger.ac.uk>
2019-12-05 14:21:02 +0000

    update

--- bin/fastq-dump_.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-
-fastq-dump --outdir fastq --gzip --split-3 sra/$1/$1.sra
--- bin/getSRA.sh
+++ bin/getSRA.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-
-while getopts ":l:" opt; do
+while getopts ":l:" opt; 
+do
   case $opt in
     l)
       cat $OPTARG | xargs -n 1 bash prefetch_.sh >&2      
@@ -11,13 +11,18 @@ while getopts ":l:" opt; do
   esac
 done
 
-while getopts ":l:" opt; do
-  case $opt in
-    l)
-      cat $OPTARG | xargs -n 1 bash fastq-dump_.sh >&2
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      ;;
-  esac
+COUNT_VMS=6
+(( STATUS = 100 / COUNT_VMS ))
+(( MODULO = 100 % COUNT_VMS ))
+COUNTER=1
+
+for file in sra/*
+do
+  (( DIFF = 100 - (TOTAL = STATUS * COUNTER++ + MODULO )))
+  clear
+  line="[$(yes "=" | head -n $TOTAL)$(yes " " | head -n $DIFF)] $TOTAL%"
+  echo -n -e "$line" | tr -d "\n"
+  sleep 2
+  n=$(basename $file)
+  fastq-dump --outdir fastq --gzip --split-3 sra/$n/$n.sra
 done
