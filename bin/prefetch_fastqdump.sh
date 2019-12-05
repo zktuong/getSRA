6de54b3720f0c8a80f024e7faa6cc0cf5ddc8dd5
Kelvin <kt16@sanger.ac.uk>
2019-12-05 13:45:36 +0000

    Update prefetch_fastqdump.sh

--- bin/prefetch_fastqdump.sh
+++ bin/prefetch_fastqdump.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 prefetch -v -O sra $1
-fastq-dump --outdir fastq --gzip --skip-technical --readids --read-filter pass --dumpbase --split-files --clip sra/$1.sra
+fastq-dump --outdir fastq --gzip --split-3 sra/$1/$1.sra
